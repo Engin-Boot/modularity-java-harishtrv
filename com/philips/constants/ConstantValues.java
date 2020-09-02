6b1c886c443f43af8a3b17403455ed0115ef4bcb
harishtrv <61978004+harishtrv@users.noreply.github.com>
2020-09-02 08:49:23 +0530

    Create ConstantValues.java

---
+++ com/philips/constants/ConstantValues.java
@@ -0,0 +1,12 @@
+package com.philips.constants;
+
+public class ConstantValues {
+    public final static String MajorColorNames[] = {
+            "White", "Red", "Black", "Yellow", "Violet"
+    };
+    public final static int numberOfMajorColors = MajorColorNames.length;
+    public final static String MinorColorNames[] = {
+            "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    public final static int numberOfMinorColors = MinorColorNames.length;
+}
